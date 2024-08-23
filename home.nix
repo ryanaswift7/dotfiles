7ca4d2a603592d14cb5dcdbec19d3b85017711cf
ryan <ryan@pop-os.lan>
2024-08-23 15:06:37 -0700

    added btop pkg

--- home.nix
+++ home.nix
@@ -66,6 +66,7 @@
     clipman
     starship
     gh
+    btop
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
