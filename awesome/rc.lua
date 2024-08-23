9592ff32c273c9a30d1b29275475c7210b9dcce6
ryan <ryan@pop-os.lan>
2024-08-22 21:13:33 -0700

    remove debian menu from awesome config

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -20,7 +20,7 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 require("awful.hotkeys_popup.keys")
 
 -- Load Debian menu entries
-local debian = require("debian.menu")
+-- local debian = require("debian.menu")
 local has_fdo, freedesktop = pcall(require, "freedesktop")
 
 -- {{{ Error handling
